4225441080b29031c6a9166f922214fbf089dc65
p.jayanth.reddy <p.jayanth.reddy@HDC3-D-451R22W.dir.svc.accenture.com>
2018-12-21 18:02:55 +0530

    Flowable config

---
+++ src/main/java/org/flowable/FlowableConfigApplication.java
@@ -0,0 +1,14 @@
+package org.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableConfigApplication.class, args);
+	}
+
+}
+
